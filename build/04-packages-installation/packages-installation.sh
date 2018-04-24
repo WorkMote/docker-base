90690e18702ce56bbe59b24358db2f77d5d74605
Daniel Dalgo <dalguete@gmail.com>
2018-04-23 23:44:27 -0500

    Add netcat command

--- build/04-packages-installation/packages-installation.sh
+++ build/04-packages-installation/packages-installation.sh
@@ -8,37 +8,38 @@ apt-get update
 # blessing, you know!
 apt-get install -y \
   bash-completion \
-	bindfs \
-	`# build-essential` \
-	bsdmainutils \
-	debconf-utils \
-	dnsutils \
-	iproute2 \
-	iputils-ping \
-	`# keychain` \
+  bindfs \
+  `# build-essential` \
+  bsdmainutils \
+  debconf-utils \
+  dnsutils \
+  iproute2 \
+  iputils-ping \
+  `# keychain` \
   less \
   locales \
-	man-db \
-	nano \
-	openssh-server \
-	python-software-properties \
-	rar \
-	software-properties-common \
-	squashfs-tools \
-	squashfuse \
-	snapd \
-	sudo \
-	supervisor \
-	telnet \
-	tzdata \
-	unrar \
-	zip
+  man-db \
+  nano \
+  netcat-openbsd \
+  openssh-server \
+  python-software-properties \
+  rar \
+  software-properties-common \
+  squashfs-tools \
+  squashfuse \
+  snapd \
+  sudo \
+  supervisor \
+  telnet \
+  tzdata \
+  unrar \
+  zip
 
 # Snap packages requirements installation. In general these packages can be installed by processes
 # triggered when the snap is set, but that could require a new apt-get complete process, possibly ending
 # in a not so fast container start, etc. Also, putting them here help us control and understand structure
 # better.
 apt-get install -y \
-	bindfs \
-	git
+  bindfs \
+  git
 
